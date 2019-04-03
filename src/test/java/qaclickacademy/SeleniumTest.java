661191dd79ea233347ad3cea8b99d6e65cbd60c7
Sharma <mahima-s@dryicelabs.com>
2019-04-03 16:18:41 +0530

    adding mvn project

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	//testng xml file from Maven
+	// how to execute all test cases from Test folder  - mvn test
+
+public static void main(String[] args){
+
+System.out.println("BrowserAUtomation");
+}	
+	
+	
+	
+	
+}
